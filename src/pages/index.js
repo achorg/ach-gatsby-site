ec94503975017c710fec39b0ef31750617e2dfb9
Ivy Rose <hi@ivyrose.me>
2023-07-05 20:28:37 -0400

    Fixed homepage browser title

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ export default Index
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="All posts" />
+export const Head = () => <Seo title="Home" />
 
 export const pageQuery = graphql`
   {
