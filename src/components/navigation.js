21c53114667b48cb3d20e8a46ee0a84e763973d1
Ivy Rose <hi@ivyrose.me>
2023-07-11 11:03:07 -0400

    More navigation refactoring and bug fixes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,8 +3,6 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import { FaBars, FaTimes, FaChevronDown, FaChevronUp } from "react-icons/fa"
 
 const Navigation = () => {
-  const [ opened, setOpened ] = useState(false);
-  
   const query = useStaticQuery(
     graphql`
       {
@@ -26,44 +24,45 @@ const Navigation = () => {
   
   const { menuLinks } = query.site.siteMetadata;
   
-  const handleMenuToggle = () => setOpened(!opened);
-  
   return (
-    <nav className={opened ? "primary-nav menu-open" : "primary-nav"}>
-      <button
-        className="menu-toggle"
-        aria-label="Toggle menu"
-        aria-expanded={opened}
-        onClick={handleMenuToggle}
-      >
-        {
-          !opened ?
-          <FaBars className="icon" aria-hidden="true" /> :
-          <FaTimes className="icon" aria-hidden="true" />
-        }
-      </button>
+    <nav className="primary-nav">
       <NavigationLevel links={menuLinks} />
     </nav>
   );
 }
 
 const NavigationLevel = ({ links, isRoot = true }) => {
-  const [ opened, setOpened ] = useState(isRoot);
+  const [ opened, setOpened ] = useState(false);
   
   const handleMenuToggle = () => setOpened(!opened);
   
   return (
     <>
-      { !isRoot &&
+      { isRoot ?
+        <button
+          className="menu-toggle"
+          aria-label="Toggle menu"
+          aria-expanded={opened}
+          onClick={handleMenuToggle}
+        >
+          {
+            !opened ?
+            <FaBars className="icon" aria-hidden="true" /> :
+            <FaTimes className="icon" aria-hidden="true" />
+          }
+        </button> :
         <button
           className="submenu-toggle"
-          onClick={handleMenuToggle}
           aria-label="Toggle submenu"
           aria-expanded={opened}
-        >{ opened ?
-          <FaChevronUp className="icon" aria-hidden="true" /> :
-          <FaChevronDown className="icon" aria-hidden="true" />
-        }</button>
+          onClick={handleMenuToggle}
+        >
+          {
+            !opened ?
+            <FaChevronDown className="icon" aria-hidden="true" /> :
+            <FaChevronUp className="icon" aria-hidden="true" />
+          }
+        </button>
       }
       <ul className={opened ? "submenu-open" : ""}>
         {links.map(link => (
