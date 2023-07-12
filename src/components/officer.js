edfc1ad4a969e9912fe738509960fec77fb6b967
Ivy Rose <hi@ivyrose.me>
2023-07-11 21:10:34 -0400

    Redesigned officers and council page

---
+++ src/components/officer.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby";
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image";
+import { FaEnvelope, FaGlobe } from "react-icons/fa";
+
+const Officer = ({
+  name,
+  imageSrc,
+  position,
+  affiliation,
+  website,
+  email
+}) => {
+  const query = useStaticQuery(graphql`
+    query {
+      allFile(filter: {relativePath: {glob: "officers/*"}}) {
+        nodes {
+          name
+          childImageSharp {
+            gatsbyImageData(width: 100, height: 100)
+          }
+        }
+      }
+    }
+  `);
+  
+  const image = getImage(query.allFile.nodes
+    .find(image => image.name === imageSrc));
+  
+  return (
+    <article className="officer-single">
+      {
+        image ?
+        <GatsbyImage
+          image={ image }
+          className="officer-image"
+          alt={`${name} headshot`}
+        /> :
+        <StaticImage
+          src="../images/no-avatar.jpg"
+          className="officer-image"
+          width="100"
+          height="100"
+          alt="Generic avatar"
+        />
+      }
+      <h3 className="officer-name">{ name }</h3>
+      <div className="officer-content">
+        {
+          position &&
+          <p className="officer-subtitle">{ position }</p>
+        }
+        {
+          affiliation &&
+          <p className="officer-description">{ affiliation }</p>
+        }
+      </div>
+      <div className="officer-socials">
+        {
+          website &&
+          <a href={ website } target="_blank" rel="noreferrer">
+            <FaGlobe className="icon" aria-hidden="true" />
+          </a>
+        }
+        {
+          email &&
+          <a href={`mailto:${email}`}>
+            <FaEnvelope className="icon" aria-hidden="true" />
+          </a>
+        }
+      </div>
+    </article>
+  );
+};
+
+export default Officer;
