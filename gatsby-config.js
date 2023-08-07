cfb1e18e2a66b6b2bd9c1842ef4e6669e798e0db
Ivy Rose <hi@ivyrose.me>
2023-08-07 12:11:44 -0400

    Updated favicon to new logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -199,11 +199,8 @@ module.exports = {
                 short_name: `ACH`,
                 start_url: `/`,
                 background_color: `#ffffff`,
-                // This will impact how browsers show your PWA/website
-                // https://css-tricks.com/meta-theme-color-and-trickery/
-                // theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                display: `browser`,
+                icon: `src/images/favicon.png`,
             },
         },
         {
